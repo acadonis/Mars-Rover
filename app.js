234db2680416522d3b0f86516f98968a74b8b33b
Alexis Adonis <acadonis@gmail.com>
2019-12-09 16:59:12 +0000

    refactoring, function tests not working in Mocha

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-
-
-//Rover 1 positioning
-
-//inputs
 const roverOneStartString = '1 2 N'
 
 const roverOneStringInstructions = 'LMLMLMLMM'
 
-const roverTwoStartString = '3 3 E'
-
-const roverTwoStringInstructions = 'MMRMMRMRRM'
-
+// const roverTwoStartString = '3 3 E'
+//
+// const roverTwoStringInstructions = 'MMRMMRMRRM'
+//
 const gridString= '5 5'
 
 //split into array
@@ -21,12 +16,9 @@ const makePositionArray = roverStartString =>  {
   return roverPositionArray
 }
 
-const roverOnePositionArray = makePositionArray(roverOneStartString)
-console.log(roverOnePositionArray)
+const roverPositionArray = makePositionArray(roverOneStartString)
 
-const roverTwoPositionArray = makePositionArray(roverTwoStartString)
-console.log(roverTwoPositionArray)
-
+console.log(roverPositionArray)
 
 //deconstruct arrays into values then create Rover object
 
@@ -34,6 +26,8 @@ const createRoverObject = roverArray => {
   const [roverEastingString, roverNorthingString, roverDirectionUncoverted] = roverArray
   const roverEasting = parseInt(roverEastingString, 10)
   const roverNorthing = parseInt(roverNorthingString, 10)
+
+  // NESW conversion function
 
   let roverConvert = direction => {
     switch(true) {
@@ -64,53 +58,40 @@ const createRoverObject = roverArray => {
   return roverObject
 }
 
-const roverTwoObject = createRoverObject(roverOnePositionArray)
-console.log(roverTwoObject)
-// convert grid ref to integer
+const roverObject = createRoverObject(roverPositionArray)
 
+console.log(roverObject)
 
+//Rover 1 instructions function
 
-
-// NESW conversion function
-
-
-//reconvert back to NESW function - ungainly
-
-let roverReconvert = direction => {
-  switch(true) {
-    case direction === 1:
-      roverReconvert = 'N'
-      break
-    case direction === 2:
-      roverReconvert = 'E'
-      break
-    case direction === 3:
-      roverReconvert = 'S'
-      break
-    case direction === 4:
-      roverReconvert = 'W'
-  }
+const roverInstructionsFunction = roverStringInstructions => {
+  const roverArrayInstructions = roverStringInstructions.split('')
+  return roverArrayInstructions
 }
 
+const roverInstructions = roverInstructionsFunction(roverOneStringInstructions)
+console.log(roverInstructions)
 
-//Rover 1 instructions
+//create grid
 
+const gridArrayCalculate = gridSize => {
+  const gridSizeArray = gridSize.split(' ')
+  const [maxWidthString, maxHeightString] = gridSizeArray
 
+  //convert grid ref to integer
+  const maxWidth = parseInt(maxWidthString, 10)
+  const maxHeight = parseInt(maxHeightString, 10)
 
-const roverOneInstructions = roverOneStringInstructions.split('')
+  const gridObject = {
+    maxHeight: maxHeight,
+    maxWidth: maxWidth
+  }
+  return gridObject
+}
 
-//grid sizing
+const grid = gridArrayCalculate(gridString)
 
-
-
-const gridArray = gridString.split(' ')
-
-const [maxWidthString, maxHeightString] = gridArray
-
-//convert grid ref to integer
-const maxWidth = parseInt(maxWidthString, 10)
-const maxHeight = parseInt(maxHeightString, 10)
-
+console.log(grid)
 
 //fuction to move rover
 
@@ -119,38 +100,64 @@ const moveRoverFunction = movementArray => {
     console.log('Function time!')
     switch (true) {
       case movementArray[i] === 'M':
-        if(roverOneDirection === 1 && roverOneNorthing + 1 <= maxHeight) {
-          roverOneNorthing += 1
-        } else if(roverOneDirection === 2 && roverOneEasting + 1 <= maxWidth){
-          roverOneEasting +=1
-        } else if(roverOneDirection === 3 && roverOneNorthing - 1 >= 0){
-          roverOneNorthing -=1
-        } else if (roverOneDirection === 4 && roverOneEasting - 1 >= 0){
-          roverOneEasting -=1
+        if(roverObject.direction === 1 && roverObject.northing + 1 <= grid.maxHeight) {
+          roverObject.northing += 1
+        } else if(roverObject.direction === 2 && roverObject.easting + 1 <= grid.maxWidth){
+          roverObject.easting +=1
+        } else if(roverObject.direction === 3 && roverObject.northing - 1 >= 0){
+          roverObject.northing -=1
+        } else if (roverObject.direction === 4 && roverObject.easting - 1 >= 0){
+          roverObject.easting -=1
         }
         break
       case movementArray[i] === 'L':
-        if (roverOneDirection - 1 === 0) {
-          roverOneDirection = 4
+        if (roverObject.direction - 1 === 0) {
+          roverObject.direction = 4
         } else
-          roverOneDirection -= 1
+          roverObject.direction -= 1
         break
       case movementArray[i] === 'R':
-        if (roverOneDirection + 1 === 5) {
-          roverOneDirection = 1
+        if (roverObject.direction + 1 === 5) {
+          roverObject.direction = 1
         } else
-          roverOneDirection += 1
+          roverObject.direction += 1
     }
-    console.log('northing', roverOneNorthing)
-    console.log('easting', roverOneEasting)
-    console.log('direction', roverOneDirection)
+    console.log('northing', roverObject.northing)
+    console.log('easting', roverObject.easting)
+    console.log('direction', roverObject.direction)
   }
-  roverOneDirectionReconvert(roverOneDirection)
 
-  const result = roverOneEasting + ' ' + roverOneNorthing + ' ' + roverOneDirection
-  console.log(result)
+  //reconvert back to NESW function - ungainly
+
+  let roverReconvert = direction => {
+    switch(true) {
+      case direction === 1:
+        roverReconvert = 'N'
+        break
+      case direction === 2:
+        roverReconvert = 'E'
+        break
+      case direction === 3:
+        roverReconvert = 'S'
+        break
+      case direction === 4:
+        roverReconvert = 'W'
+    }
+    return roverReconvert
+  }
+
+  roverObject.direction = roverReconvert(roverObject.direction)
+
+  const result = roverObject.easting + ' ' + roverObject.northing + ' ' + roverObject.direction
   return result
 
 }
 
-moveRoverFunction(roverOneInstructions)
+console.log(moveRoverFunction(roverInstructions))
+
+module.exports = {
+  makePositionArray,
+  createRoverObject,
+  roverInstructionsFunction,
+  roverOneStartString
+}
--- test/tests_spec.js
+++ test/tests_spec.js
@@ -5,14 +5,52 @@
 const chai = require('chai')
 const expect = chai.expect
 const app = require('../app')
-const moveRoverFunction = app.moveRoverFunction
-const roverOneInstructions = app.roverOneInstructions
+const makePositionArray = app.makePositionArray
+const createRoverObject = app.createRoverObject
+const roverInstructionsFunction = app.roverInstructionsFunction
+const roverOneStartString = app.roverOneStartString
+// const roverOneInstructions = app.roverOneInstructions
 
 
 
 describe('Rover movement tests', () => {
 
-  it('Should return correct end result with movement instructions, rest still hardcoded', () => {
-    expect(moveRoverFunction(roverOneInstructions)).to.eq('1 5 E')
+  it('Should return an array of strings', () => {
+    expect(makePositionArray('1 5 E')).to.be.a('array')
+  })
+
+  it('Should return 1 2 N', () => {
+    expect(roverOneStartString).to.equal('1 2 N')
+  })
+
+  it('Should return an array of strings which matches input string', () => {
+    expect(makePositionArray('1 5 E')).to.equal(['1', '5', 'E'])
+  })
+
+  it('Should change strings into numbers and create rover object', () => {
+    expect(createRoverObject(['1', '5', 'E'])).to.equal(
+      {
+        easting: 1,
+        northing: 5,
+        direction: 2
+      }
+    )
+  })
+
+  it('Should return an array of strings', () => {
+    expect(roverInstructionsFunction('LMLMLMLMM')).to.be.a('array')
+  })
+
+  it('Should return movement string to movement array', () => {
+    expect(roverInstructionsFunction('LMLM')).to.equal(['L', 'M', 'L', 'M'])
   })
 })
+
+// var expect = require('chai').expect
+//   , foo = 'bar'
+//   , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+//
+// expect(foo).to.be.a('string');
+// expect(foo).to.equal('bar');
+// expect(foo).to.have.lengthOf(3);
+// expect(beverages).to.have.property('tea').with.lengthOf(3);
